a538b10e52d7b545f9d27c82e055786e9cc9bcf9
ouhell <oussamabarca200@gmail.com>
2023-07-19 17:42:39 +0100

    remove out

--- out/handlers/UrlHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UrlHandler = (req, res, next) => {
-    if (req.url.endsWith("/") && req.url.length > 1) {
-        req.url = req.url.substr(0, req.url.length - 1);
-    }
-    next();
-};
-exports.default = UrlHandler;
--- out/schema/request/RequestModel.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const RequestSchema = new mongoose_1.Schema({
-    requester: { type: mongoose_1.Types.ObjectId, required: true },
-    destinator: { type: mongoose_1.Types.ObjectId, required: true },
-    date: { type: Date, default: Date.now },
-});
-exports.default = (0, mongoose_1.model)("request", RequestSchema);
